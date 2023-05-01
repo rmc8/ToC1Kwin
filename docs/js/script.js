1ec4f89b58b88e8c7d019b271cd614ac75b9a922
rmc8 <kmprmc@gmail.com>
2023-05-01 13:20:53 +0900

    [ADD] accent color

--- docs/js/script.js
+++ docs/js/script.js
@@ -13,3 +13,31 @@ $(function () {
     }
   });
 });
+
+let app = new Vue({
+  el: "#links",
+  data: {
+    links: [
+      {
+        label: "@ToC1Kwin",
+        url: "https://twitter.com/ToC1Kwin",
+        icon: "https://abs.twimg.com/favicons/twitter.ico",
+      },
+      {
+        label: "@rmc_km",
+        url: "https://twitter.com/rmc_km",
+        icon: "https://abs.twimg.com/favicons/twitter.ico",
+      },
+      {
+        label: "GitHub",
+        url: "https://github.com/rmc8/",
+        icon: "https://github.githubassets.com/favicon.ico",
+      },
+      {
+        label: "note",
+        url: "https://note.com/toc1kwin",
+        icon: "https://assets.st-note.com/poc-image/manual/note-common-images/production/svg/production.ico",
+      },
+    ],
+  },
+});
